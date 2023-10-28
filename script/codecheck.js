efc7f6b298e36d31a6a6d784cc763a0029704122
ArtKaiden <116389335+ArtKaiden@users.noreply.github.com>
2023-10-28 22:52:20 +0200

    Add files via upload

---
+++ script/codecheck.js
@@ -0,0 +1,19 @@
+const elevatorButton = document.querySelector(".elevator-button");
+const article = document.querySelector("article");
+const code = document.getElementById("code");
+const result = document.getElementById("result");
+const up = document.querySelector(".up");
+
+elevatorButton.addEventListener("click",() =>{
+  article.classList.remove("invisible");
+});
+
+function CheckCode(){
+  if (code.value == 5844) {
+    result.textContent = "Je lanceercode is correct";
+    up.classList.remove("invisible");
+  } else {
+    result.textContent = "Verkeerde lanceercode";
+    up.classList.add("invisible");
+  }
+}
